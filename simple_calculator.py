e0150efc232e629cec5a5f18626bc3b01a9bd9d0
Kmolina009 <45610174+Kmolina009@users.noreply.github.com>
2019-06-06 22:29:22 -0500

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,35 @@
+#Simple Calculator
+
+# I want to build a calculator that will allow me to input two values
+
+#prototype
+
+def calculator():
+    num1 = input("Give me a number")
+    num2 = input("Give me another")
+    print("What is the answer you")
+    #Prompt the user to select something
+    input("What would you like to do?")
+    # Add = print("Add?") 
+    # Subtract = print("Subtract?") 
+    # Multiply = print("Multiply?") 
+    # Divide = print("Divide?") 
+    # Exponentiate = print("Exponentiate?") 
+        if(Add)
+     elif(Subtract):
+         num1 - num2 
+         num1 + num2
+     elif(Multiply) 
+         num1 * num2
+     elif(Divide) 
+         num1 / num2
+     elif(Exponentiate)
+         num1 ** num2 
+     # + - / * ** could I write a square root operator?
+#     #retrun answer
+#     #if the user wants to cancel, then they should input quit
+      # Otherwise, it will persist(loop?) 
+#     #
+# #Then input a the operator I want to use on said numbers
+
+# #Then return said numbers
